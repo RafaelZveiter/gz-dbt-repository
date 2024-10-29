00687b5c57602acdcaae878e55e1f28139e6400a
rzveitern@gmail.com <rzveitern@gmail.com>
2024-10-29 14:39:06 +0000

    saved orders operational

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,19 @@
+WITH op AS(
+SELECT
+    orders.*
+    , ship.shipping_fee
+    , ship.ship_cost
+    , ship.logcost
+FROM {{ ref('int_orders_margin') }} AS orders
+LEFT JOIN {{ ref('stg_raw__ship') }} AS ship
+USING (orders_id)
+)
+SELECT
+    orders_id
+    , date_date
+    , ROUND(margin + shipping_fee - logcost - ship_cost, 2) AS operational_margin
+    , quantity
+    , margin
+    , revenue
+FROM op
+ORDER BY date_date
