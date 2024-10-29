49454da398248f75e3adc19634afde006857df77
rzveitern@gmail.com <rzveitern@gmail.com>
2024-10-29 15:05:39 +0000

    finance day added

---
+++ models/mart/finance_day.sql
@@ -0,0 +1,16 @@
+SELECT
+    date_date
+    , COUNT(orders_id) AS nb_transactions
+    , ROUND(SUM(revenue), 1) AS revenue
+    , ROUND(SAFE_DIVIDE(SUM(revenue), COUNT(orders_id)), 1) AS average_basket
+    , ROUND(SUM(margin), 1) AS margin
+    , ROUND(SUM(operational_margin), 1) AS operational_margin
+    , ROUND(SUM(purchase_cost), 1) AS purchase_cost
+    , ROUND(SUM(shipping_fee), 1) AS shipping_fee
+    , ROUND(SUM(logcost), 1) AS log_cost
+    , SUM(quantity) AS quantity
+FROM {{ ref('int_orders_operational') }}
+GROUP BY date_date
+ORDER BY date_date DESC
+
+
